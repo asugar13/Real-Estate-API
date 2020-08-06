0efafab0f14fcdfe3641a5baee5b5f706e135fb2
asugar13 <asier.ugarteche@gmail.com>
2020-08-06 19:27:38 -0400

    started mongo implementatio

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/realstatedb"
+mongo = PyMongo(app)
 
-@app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/", methods=['GET','POST'])
+def index():
+    if (request.method== "POST"):
+        return "you posted me"
+
+    else:
+        return "you got me"
+    
+@app.route("/house", methods=["POST"])
+def create_house():
+    
+    
 
 #if __name__ == '__main__':
  #   app.run(debug=True)
