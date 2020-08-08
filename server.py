b62f63db01a29709351e82fe8958ccb06fc74e0d
asugar13 <asier.ugarteche@gmail.com>
2020-08-08 14:14:15 -0400

    implemented sessions

--- server.py
+++ server.py
@@ -1,43 +1,52 @@
-from flask import Flask, request, Response, render_template
+from flask import Flask, request, Response, render_template, session, redirect, url_for, jsonify
 from flask_pymongo import PyMongo
 from bson import json_util
 from bson.objectid import ObjectId
 
-import os;
-
-
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/realestatedb"
 mongo = PyMongo(app)
+app.config["SECRET_KEY"] = 'sdn86shbmsdACs'
 
-
-@app.route("/", methods=['GET','POST'])
-def index():
-    if (request.method== "POST"):
-        return "you posted me"
-
-    else:
-        return "you got me"
+        
+@app.route("/signup", methods=["POST"])
+def signup():
+    username = request.form["username"] 
+    password = request.form["password"]     #get user name and pw from form data
+    user_id = mongo.db.users.insert({"username": username,
+                            "password": password})  #save user to database
+    new_user = mongo.db.users.find_one({"_id": user_id}) #make sure user was saved
+    result = {"user": new_user['username'] + " registered"}
+    return jsonify({'result' : result})
+           
     
-@app.route("/login", methods=["POST", "GET"])
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    #return redirect(url_for(''))
+    return "logged out"
+    
+@app.route("/", methods=["POST", "GET"])
 def login():
     if request.method == "GET":
-        print(os.getcwd());
         return render_template("login.html")
-    
-@app.route("/houses", methods=["GET"])
-def get_houses(): 
-    houses = mongo.db.realestates.find()
-    response = json_util.dumps(houses)
-    return Response(response, mimetype='application/json')
-    
+    elif request.method == "POST": 
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        user = mongo.db.users.find_one({"username": username, "password": password})
+        if user:   #if user is in the database so variable is not null
+            session["username"] = username
+            return "logged in"
+            #return redirect(url_for("profile")) #redirect    
     
 @app.route('/<id>/houses', methods=["GET"])
 def get_city_houses(id):
-    houses = mongo.db.realestates.find({"city": id})
-    response = json_util.dumps(houses)
-    return response
-    
+    if "username" in session:
+        houses = mongo.db.realestates.find({"city": id})
+        response = json_util.dumps(houses)
+        return Response(response, mimetype='application/json')
+    return "hey"
 
 if __name__ == '__main__':
    app.run(debug=True)
