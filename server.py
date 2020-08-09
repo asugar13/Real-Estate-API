2a83dc243bcd9e2b07f4b5f87ec84d3a180d2b7e
asugar13 <asier.ugarteche@gmail.com>
2020-08-09 09:24:33 -0400

    check if user exists - /signup

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, Response, render_template, session, redirect, url_for, jsonify
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo #mongodb implementation for flask
 from bson import json_util
 from bson.objectid import ObjectId
 from datetime import timedelta
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt  #module to encrypt user passwords
 
 #from pymongo import MongoClient, IndexModel
 #from pymongoext import *
@@ -33,12 +33,16 @@ app.pemanent_session_lifetime = timedelta(minutes=5)
 def signup():
     username = request.form["username"] 
     password = bcrypt.generate_password_hash(request.form["password"]).decode('utf-8')     #get user name and pw from form data
+    #make sure user does not exist
     
-    user_id = mongo.db.users.insert({"username": username,
+    check_user = mongo.db.users.find({"username": username})
+    if check_user.count() == 0:
+            user_id = mongo.db.users.insert({"username": username,
                             "password": password})  #save user to database
-    new_user = mongo.db.users.find_one({"_id": user_id}) #make sure user was saved
-    result = {"user": new_user['username'] + " registered"}
-    return jsonify({'result' : result})
+            new_user = mongo.db.users.find_one({"_id": user_id}) #make sure user was saved
+            result = {"user": new_user['username'] + " registered"}
+            return jsonify({'result' : result})
+    return "username already exists!"
            
     
 @app.route('/logout')
@@ -66,12 +70,25 @@ def login():
         else:
             return "user does not exist"
     
-@app.route('/<id>/houses', methods=["GET"])
+@app.route('/<id>/houses', methods=["GET", "POST"])  #TODO: IMPLEMENT POST FOR USERS TO ADD HOUSE
 def get_city_houses(id):
     if "username" in session:
-        houses = mongo.db.realestates.find({"city": id})
-        response = json_util.dumps(houses)
-        return Response(response, mimetype='application/json')
+        if request.method == "GET":
+            houses = mongo.db.realestates.find({"city": id})
+            if houses.count() == 0:
+                return "No houses found in location: " + id
+  
+            response = json_util.dumps(houses)
+            return Response(response, mimetype='application/json')
+        if request.method == "POST":
+            name = request.form["name"]
+            description = request.form["description"]
+            place_type = request.form["type"]
+            bedrooms = request.form["bedrooms"]
+            additional_info = request.form["additional info"]
+            owner = session["username"]
+            city = id
+            return "ge"
     return "You are not logged in or your session is expired"
 
 @app.route("/profile", methods=["POST", "GET"])
@@ -80,6 +97,9 @@ def profile_handler():
         if request.method == "POST": 
             
             print(request.form)
+            username = None#finish this
+            first_name = None
+            last_name = None
             for i in request.form:
                 print(i)
             username = request.form["username"]
@@ -109,11 +129,24 @@ def profile_handler():
             
             response = json_util.dumps(user)
             return Response(response, mimetype='application/json')            
-            
-                                           
-        
         
     return "You are not logged in or your session is expired"
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    #message = {
+        #"message": "Resource not found: " + request.url
+        #}
+    #return message
+    response = jsonify({
+        "message": "Resource not found: " + request.url,
+        "status": 404
+        })
+    response.status_code = 404
+    return response
+        
+
 
 if __name__ == '__main__':
    app.run(debug=True)
